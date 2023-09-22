bcf1fb35c6ceda6f7addb0bb9f4baeb693ddda6c
Ralf Mengel <ralf.mengel@wobcom.de>
2023-09-22 19:05:34 +0200

    init

--- changeStreamInfo.php
+++ changeStreamInfo.php
@@ -10,33 +10,33 @@ include ('inc/classes/mysql_db.php');
 $config_name = 'changeStreamInfo';
 
 $stream = new twitchapi($config_name);
-$stream->refresh_token($config_name);
+$stream->refresh_token();
 
 if (!empty($_SESSION['access_token'])) {
-	#echo "<br />Debug - login abgeschlossen <br />";    
+	#echo "<br />Debug - login abgeschlossen <br />";
     $streamInfos 		= new twitchforms($config_name);
-	
+
 	$broadcasterInfo  	= $stream->getBroadcasterInfos();
 	$channelInfo 		= $stream->getChannelInfos($broadcasterInfo['broadcaster_id']);
 	$streamInfo         = $stream->getStreamInfo($broadcasterInfo['broadcaster_id']);
 	$followerCount      = $stream->getFollowerCount($broadcasterInfo['broadcaster_id']);
-	
+
 	# alle Arrays aus den einzelnen API Calls werden vermischt
 	$Infos 				= array_merge($broadcasterInfo, $channelInfo, $streamInfo, $followerCount);
 	$streamInfosForm	= $streamInfos->editStreamInfos($Infos);
-	
+
 	$form 				= new twitchforms($config_name);
 	echo $form->design($streamInfosForm);
-	
+
 } else {
 	#echo "<br />Debug - login benoetigt <br />";
-	
+
     $login 			= new twitchforms($config_name);
 	$loginForm 		= $login->showLoginForm();
-	
+
 	$form 			= new twitchforms($config_name);
 	echo $form->design($loginForm);
-	
+
 	$stream->login();
 }
 
